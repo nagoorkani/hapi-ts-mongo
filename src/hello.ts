90905cabf52c5240f0045bd1d3b5877a2a7ad5d8
nagoor kani <rasheeeds@gmail.com>
2022-05-29 08:30:27 -0500

    Added swagger API docs
    Signed-off-by: nagoor kani <rasheeeds@gmail.com>

--- src/hello.ts
+++ src/hello.ts
@@ -1,20 +1,34 @@
 import { Request } from '@hapi/hapi';
-
-const index = () => {};
+import Joi from 'joi';
 
 export const HelloRoutes = [
   {
     method: 'GET',
     path: '/hello',
-    handler: () => {
-      return 'Hello! Welcome';
+    options: {
+      tags: ['api'],
+      handler: () => {
+        return 'Hello! Welcome';
+      },
     },
   },
   {
     method: 'GET',
     path: '/hello/{name}',
-    handler: (request: Request) => {
-      return `Hello! Welcome ${request.params.name}`;
+    options: {
+      handler: (request: Request) => {
+        return `Hello! Welcome ${request.params.name}`;
+      },
+      description: 'Get todo',
+      notes: 'Returns a greetings with {name} param passed.',
+      tags: ['api'],
+      validate: {
+        params: Joi.object({
+          name: Joi.string()
+            .required()
+            .description('the {name} for the greetings.'),
+        }),
+      },
     },
   },
 ];
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import { Server } from '@hapi/hapi';
 import * as config from './config/dev.json';
 import { HelloRoutes } from './hello';
 import { productsRoutes } from './products';
+import * as HapiSwagger from 'hapi-swagger';
+import Vision from '@hapi/vision';
+import Inert from '@hapi/inert';
+import { version as APP_VERSION } from '../package.json';
 
 export let server: Server;
 
@@ -11,6 +15,29 @@ export const init = async (): Promise<Server> => {
     port: config.APP_PORT,
     host: config.APP_HOST,
   });
+
+  // swagger
+  const swaggerOptions: HapiSwagger.RegisterOptions = {
+    info: {
+      title: 'Test API Documentation',
+      version: APP_VERSION,
+    },
+  };
+
+  const plugins: Array<Hapi.ServerRegisterPluginObject<any>> = [
+    {
+      plugin: Inert,
+    },
+    {
+      plugin: Vision,
+    },
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    },
+  ];
+
+  await server.register(plugins);
 
   // adding routes - inline
   server.route({
